a1c6baedc0d781e13728cdc79f22ade40287d985
yuinin <yuis2teddy@gmail.com>
2022-10-24 14:09:36 +0900

    ログイン、ログアウトの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 
 end
