07f1e29587e485e7ac5d5eaa98355677dfc716e2
yuinin <yuis2teddy@gmail.com>
2022-10-28 15:51:37 +0900

    マイグレーションファイルの編集

--- db/migrate/20220926002317_create_items.rb
+++ db/migrate/20220926002317_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string  :product_name, null: false
+      t.text    :explanation, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :shipping_cost_id, null: false
+      t.integer :from_id, null: false
+      t.integer :send_id, null: false
+      t.integer :price, null: false
+      t.references   :user_id, null: false, foreign_key: true
 
       t.timestamps
     end
