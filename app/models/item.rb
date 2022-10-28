8ed60393110dff0ae06bdd645f2d3c856eee47ec
yuinin <yuis2teddy@gmail.com>
2022-10-28 16:39:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  validates :product_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :from_id, presence: true
+  validates :send_id, presence: true
+  validates :price, presence: true
+  validates :user_id, presence: true, foreign_key: true
 end
