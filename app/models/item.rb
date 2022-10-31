01876b99882a955c1b370a59da19b360d231b3f4
yuinin <yuis2teddy@gmail.com>
2022-10-31 15:55:52 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
   validates :product_name, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true
@@ -9,6 +10,6 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, presence: true
   validates :from_id, presence: true
   validates :send_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   validates :user_id, presence: true, foreign_key: true
 end
