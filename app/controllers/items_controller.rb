fa0d186f5dae07aecf461d8f182ccd9ff20dbfd2
yuinin <yuis2teddy@gmail.com>
2022-10-31 16:13:33 +0900

    選択データの格納

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(Item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
   private
 
   def move_to_index
@@ -13,3 +27,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :from_id, :send_id, :price).merge(user_id: current_user.id)
   end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,13 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :product_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :from_id, presence: true
-  validates :send_id, presence: true
+
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :from_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :send_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+
   validates :price, presence: true, numericality: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   validates :user_id, presence: true, foreign_key: true
 end
