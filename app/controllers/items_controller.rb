de3a6021fee59c7394913d9e3bdce9a026ada600
yuinin <yuis2teddy@gmail.com>
2022-11-11 16:21:07 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_login, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit]  
 
   def index
     @item = Item.all.order(updated_at: 'DESC')
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
   
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -39,19 +37,21 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_login
     redirect_to new_user_session_path unless user_signed_in?
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     if user_signed_in?
       unless @item.user_id == current_user.id
         redirect_to action: :index
       end
     end
   end
-
 
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :from_id,
