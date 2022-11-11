baec3a33533cc24b5a32c67eaf6e7e0d5754b6d3
yuinin <yuis2teddy@gmail.com>
2022-11-11 19:11:52 +0900

    destroyアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_login, except: [:index, :show]
   before_action :move_to_index, only: [:edit]  
 
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
