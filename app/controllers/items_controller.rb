3285978f40bb0e2d77168c5de6a3d7016769bb9e
yuinin <yuis2teddy@gmail.com>
2022-11-11 15:49:43 +0900

    エラーハンドリングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_login, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @item = Item.all.order(updated_at: 'DESC')
@@ -28,8 +29,12 @@ class ItemsController < ApplicationController
   end
   
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -37,6 +42,16 @@ class ItemsController < ApplicationController
   def move_to_login
     redirect_to new_user_session_path unless user_signed_in?
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    if user_signed_in?
+      unless @item.user_id == current_user.id
+        redirect_to action: :index
+      end
+    end
+  end
+
 
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :from_id,
