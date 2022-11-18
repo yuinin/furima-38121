2c7ef515e26c74ee1116a0e5f26aa73f035b871d
yuinin <yuis2teddy@gmail.com>
2022-11-18 10:50:47 +0900

    パス変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path(@item.user_id)
+      redirect_to root_path
     else
       render :new
     end
