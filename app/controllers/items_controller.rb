6fcd47e8a69863a603238abdd3291ce589ad6639
yuinin <yuis2teddy@gmail.com>
2022-11-18 11:50:41 +0900

    pathの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to root_path
+      render :show
     else
       render :edit
     end
